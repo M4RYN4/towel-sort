22d3d856bb5a7ba972c39f857ae749b44837ab06
Maryna <rimana.ca@gmail.com>
2023-09-20 14:09:04 -0400

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 
 // You should implement your task here.
+//from [[1, 2],[3, 4],] -> [ 1, 2, 4, 3 ]
 
 module.exports = function towelSort (matrix) {
-  return [];
+    let arr = [];
+    if(Array.isArray(matrix)){//1. check if array is not undefined
+        for(let i = 0; i < matrix.length; i++){
+            if(i === 0 || i % 2 === 0){//if idx = 0 || even ->
+                arr.push(...matrix[i]);
+            }else{
+                arr.push(...matrix[i].sort((a, b) => b - a));//odd -> reverse order
+            }
+        }
+    }
+
+    return arr;
 }
